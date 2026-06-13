3b77d4d85d1ddf172a3f6699ebdccfe3b7bdad8d
saraharende1-ops <saraharende1@gmail.com>
2026-06-14 00:21:42 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@react-js/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // This forces absolute routing assets on production platforms
 })
